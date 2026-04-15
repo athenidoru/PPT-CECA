4c20d6208254042dd156f5492cff12bdf1bd6994
Christian Esteban Calderón Acurio <128630513+athenidoru@users.noreply.github.com>
2026-04-15 20:30:07 +0000

    Prueba codespace

--- juego_ppt.py
+++ juego_ppt.py
@@ -108,13 +108,3 @@ while ejecutando_juego:
         # Si no escribió ni jugar ni reglas le sale este mensaje.
         print("Opción incorrecta.")
     
-
-
-
-
-
-
-
-
-
-# prueba de cambio en github.
